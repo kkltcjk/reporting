460731b8b2da037159649b02ffed798656dad8a9
chenjiankun <chenjiankun1@huawei.com>
2016-12-24 22:23:34 +0000

    Create Test Reporting landing page
    JIRA: RELENG-165
    
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

--- utils/test/reporting/api/handlers/landing.py
+++ utils/test/reporting/api/handlers/landing.py
@@ -4,9 +4,9 @@ from tornado.escape import json_encode
 
 class FiltersHandler(RequestHandler):
     def get(self):
-        return json_encode({'status': 'SUCCESS'})
+        return self.write(json_encode({'status': 'SUCCESS'}))
 
 
 class ScenariosHandler(RequestHandler):
     def get(self):
-        return json_encode({'status': 'SUCCESS'})
+        return self.write(json_encode({'status': 'SUCCESS'}))
